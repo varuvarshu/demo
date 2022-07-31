a89f0c86ed7e3fa3da7f5964373a128cb567a368
varuvarshu <101593792+varuvarshu@users.noreply.github.com>
2022-07-31 13:34:49 +0530

    Add files via upload
    day1

---
+++ src/com/masai/problem5/Main.java
@@ -0,0 +1,10 @@
+package com.masai.problem5;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("varsa");
+        System.out.println("sanjay kumar");
+        System.out.println("neelam");
+    }
+
+}
